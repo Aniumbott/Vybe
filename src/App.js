6d841227cd741a385ce4588eb8cc0edde5e9e84e
unknown <Aniumbott>
2021-10-21 17:26:19 +0530

    just for a backup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+// Import Styles
+import "./styles/app.scss";
+// Adding components
+import Player from "./components/Player";
+import Song from "./components/Song";
+// Importt Utils
+import data from "./util";
+
 function App() {
   return (
     <div className="App">
       <h1>Vybe</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
